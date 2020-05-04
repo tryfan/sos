89de4795c32fae43dee16277ce1690abe44417fa
tryfan <nick@celebic.net>
2020-05-04 10:07:18 -0400

    masking ssl paths

--- sos/plugins/morpheus.py
+++ sos/plugins/morpheus.py
@@ -11,6 +11,7 @@ class Morpheus(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
         self.add_copy_spec("/etc/morpheus/*")
         self.add_copy_spec("/opt/morpheus/version-manifest.json")
         self.add_copy_spec("/opt/morpheus/conf/application.yml")
+        self.add_forbidden_path("/etc/morpheus/ssl/*")
         self.add_cmd_output([
             'morpheus-ctl status'
         ])
