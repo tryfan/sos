a52d8712b233a005a0d677a1df82c7f8a41afe72
tryfan <nick@celebic.net>
2020-05-04 10:11:55 -0400

    masking secrets

--- sos/plugins/morpheus.py
+++ sos/plugins/morpheus.py
@@ -12,6 +12,7 @@ class Morpheus(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
         self.add_copy_spec("/opt/morpheus/version-manifest.json")
         self.add_copy_spec("/opt/morpheus/conf/application.yml")
         self.add_forbidden_path("/etc/morpheus/ssl/*")
+        self.add_forbidden_path("/etc/morpheus/morpheus-secrets.json")
         self.add_cmd_output([
             'morpheus-ctl status'
         ])
